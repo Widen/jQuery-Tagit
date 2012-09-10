d65fc7f9bb09bcb91f5e959414e5c23c24b842bd
Ray Nicholus <rnicholus@widen.com>
2012-09-10 10:25:01 -0500

    add mousedownoutside extra outside event for first tagging example

--- javascripts/tagitdemo.js
+++ javascripts/tagitdemo.js
@@ -1,43 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#simple > ul').tagit({
-        select: true,
-        triggerKeys: ['comma', 'enter', 'space', 'semicolon', 'tab']
+        select:true,
+        triggerKeys:['comma', 'enter', 'space', 'semicolon', 'tab'],
+        extraOutsideEvents:['mousedownoutside']
     });
 
     $('#autocomplete > ul').tagit({
-        select: true,
-        triggerKeys: ['comma', 'enter', 'space', 'semicolon', 'tab'],
-        tagSource: ['one', 'two', 'three', 'four', 'five', 'six']
+        select:true,
+        triggerKeys:['comma', 'enter', 'space', 'semicolon', 'tab'],
+        tagSource:['one', 'two', 'three', 'four', 'five', 'six']
     });
 
     $('#sortable > ul').tagit({
-        select: true,
-        triggerKeys: ['comma', 'enter', 'space', 'semicolon', 'tab'],
-        sortable: 'handle'
+        select:true,
+        triggerKeys:['comma', 'enter', 'space', 'semicolon', 'tab'],
+        sortable:'handle'
     });
 
     $('#editOnClick > ul').tagit({
-        select: true,
-        triggerKeys: ['comma', 'enter', 'space', 'semicolon', 'tab'],
-        editOnClick: true
+        select:true,
+        triggerKeys:['comma', 'enter', 'space', 'semicolon', 'tab'],
+        editOnClick:true
     });
 
-    $('a.displaySelect').click(function() {
+    $('a.displaySelect').click(function () {
         var select = $(this).parent('div').find('select');
         var children = "";
-        select.children().each(function(){
+        select.children().each(function () {
             children += $(this).val() + " : " + $(this).text() + "\n";
         });
         alert("Select options [value: text]...\n\n" + children);
     });
 
-    $('ul.tagit').focusin(function() {
+    $('ul.tagit').focusin(function () {
         if (!$(this).hasClass('selected')) {
             $(this).addClass('selected');
         }
     })
-    .focusout(function(event) {
-        $(this).removeClass('selected');
-    });
+        .focusout(function (event) {
+            $(this).removeClass('selected');
+        });
 
 });
